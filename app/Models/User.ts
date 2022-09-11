98d21b2ff5093b594fe651aa3fd7c64bdefc0d9a
vitorrafaelm <vitor.rafael1518@gmail.com>
2022-09-11 20:38:44 -0300

    feat: users model

---
+++ app/Models/User.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public username: string
+
+  @column()
+  public email: string
+
+  @column()
+  public type: 'STUDENT' | 'CONTENT_MAKER'
+
+  @column()
+  public isDeleted: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
