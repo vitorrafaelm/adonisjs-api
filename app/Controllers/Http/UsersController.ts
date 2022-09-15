1ffb40e509a0cbc5841403751abf713176e83e8b
vitorrafaelm <vitor.rafael1518@gmail.com>
2022-09-15 19:31:06 -0300

    feat: created user controller and migrations

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,16 +2,17 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
+  // Router to register student and content maker, a student must have a creatorId
   public async createStudent({ request, response }: HttpContextContract) {
-    const { name, username, email, type, idCreator } = request.body()
+    const { name, username, email, typeId, creatorId } = request.body()
 
     try {
       const userCreated = await User.create({
         name,
         username,
         email,
-        type,
-        idCreator,
+        typeId,
+        creatorId,
       })
 
       return response.status(201).json(userCreated)
@@ -20,11 +21,12 @@ export default class UsersController {
     }
   }
 
+  // Content maker can list the students he registered
   public async listStudents({ params, response }: HttpContextContract) {
     const { idContentMaker } = params
 
     try {
-      const contentCreator = await User.query().where('idCreator', idContentMaker)
+      const contentCreator = await User.query().where('creator_id', idContentMaker)
 
       return response.status(201).json(contentCreator)
     } catch (error) {
---
+++ database/migrations/1663080477983_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('username').notNullable()
+      table.string('email').notNullable()
+
+      table.boolean('isDeleted').defaultTo(false).notNullable()
+      table.integer('creator_id').unsigned().references('users.id')
+      table.integer('type_id').unsigned().references('user_types.id')
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
